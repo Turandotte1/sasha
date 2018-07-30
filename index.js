95eb3929834f9fe44f01a382e012646c1f954876
My mac <mari_rychkova@yahoo.com>
2018-07-31 02:40:11 +0300

    clean

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
-
+const parser = require('body-parser');
+const newsLetter = require('./middlewares/news.js');
 
 app = express();
 
-app.get ('/',  (req, res)=>{
-	console.log('je suis le main');
-	res.sendFile(__dirname + '/index.html');
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+
+app.use('/styles', express.static('public/styles'));
+app.use('/img', express.static('public/img'));
+app.use('/icons', express.static('public/icons'));
+app.use('/js', express.static('public/js'));
+
+app.get('*', function (req, res) {
+		res.render('comingSoon');
 });
 
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    res.send('caca');
+app.post('*', function (req, res) {
+    newsLetter(req, res);
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-	console.log('j ecoute sur le port:', port)
-});
+const PORT = process.env.PORT || 8080;
+app.listen (PORT, () => console.log('Listening on port ' + PORT));
