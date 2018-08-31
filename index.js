9ebb8f79c1523faeeee0ec954930d6adea2a00a4
allauren <alexislaurentprof@gmail.com>
2018-08-31 22:09:30 +0200

    added callback

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const parser = require('body-parser');
 const newsLetter = require('./middlewares/news.js');
+const flash = require('express-flash');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 app = express();
 
@@ -9,6 +12,9 @@ app.set('view engine', 'pug');
 
 app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
+app.use(cookieParser('keyboard cat'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
 
 app.use('/styles', express.static('public/styles'));
 app.use('/img', express.static('public/img'));
@@ -16,11 +22,18 @@ app.use('/icons', express.static('public/icons'));
 app.use('/js', express.static('public/js'));
 
 app.get('/', function (req, res) {
-		res.render('comingSoon');
+		res.render('comingSoon', {messages: req.flash('info')});
 });
 
 app.post('/signup', function (req, res) {
-    newsLetter(req, res);
+    newsLetter(req, res, (err, data) =>{
+		if (err)
+		{
+			return res.redirect('/?e='+ encodeURIComponent('Incorrect username or password'));
+		}
+			res.redirect('/');
+		}
+	);
 });
 
 const PORT = process.env.PORT || 8080;
